e00b0b59d955abbdd29fd8585d8b8e7a54005a7b
ricky0143 <ricky0143@gmail.com>
2015-04-01 17:58:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendfile('./index.html');
+	res.sendfile('./public/index.html');
+});
+
+app.get('/storytime', function(req, res) {
+	res.send("<p> Hey" + req.query.name + "nice to meet you </p>");
 });
 
 app.listen(app.get('port'), function() {
